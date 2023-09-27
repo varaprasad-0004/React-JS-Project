080fe8bb9435c10830fc9279c2de29a87d1499f6
varaprasad-0004 <buddanavaraprasad@gmail.com>
2023-09-27 21:35:34 +0530

    ReactJS

---
+++ React_E-Commerce-main/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
